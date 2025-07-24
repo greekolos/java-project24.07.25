7a1d4e15e92e0a98d3d2a0076e91ac72d44d62ad
Greekolos <lte7@yandex.ru>
2025-07-24 13:08:48 +0300

    5

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -1,35 +1,40 @@
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class q2 {
+public class Test2 {
     private WebDriver driver;
 
     @BeforeEach
     void setup() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize(); // Разворачивает окно браузера
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1080");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
     @Test
     void testUserRegistration() {
-        // 1. Открытие главной страницы
         driver.get("https://practicetestautomation.com/practice-test-login/");
 
         driver.findElement(By.name("username")).sendKeys("incorrectUser");
         driver.findElement(By.name("password")).sendKeys("Password123");
         driver.findElement(By.id("submit")).click();
 
-
-
-
-        // 7. Ожидание заголовка с нужным текстом
         new WebDriverWait(driver, Duration.ofSeconds(20))
                 .until(ExpectedConditions.textToBePresentInElementLocated(
                         By.tagName("body"),
@@ -38,7 +43,6 @@ public class q2 {
 
         System.out.println("✅ Введите правильный логин!");
     }
-
-
 }
+
 
